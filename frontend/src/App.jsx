52f487f6d52f9b06e08c48df90ed28be7a597a39
DySn0512 <145582634+DySn0512@users.noreply.github.com>
2024-11-15 23:17:07 +0700

    add: LoginPage.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,6 @@
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import UserListPage from "./pages/UserListPage"; // Import UserListPage
 import { UserIcon, DocumentTextIcon, CubeIcon } from "@heroicons/react/24/outline"; // Hoặc @heroicons/react/24/solid
-
 function App() {
     return (
         <Router>
