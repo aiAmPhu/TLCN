6f20af9cbe43a5b9883e12ceb87d95cc0b1ae748
DySn0512 <145582634+DySn0512@users.noreply.github.com>
2024-12-01 20:27:40 +0700

    add: Footer

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+    return (
+      <div className="bg-gray-800 text-white text-center py-4">
+        <p>© 2024 University of Transport and Communications</p>
+      </div>
+    );
+  };
+  
+  export default Footer;
+  
