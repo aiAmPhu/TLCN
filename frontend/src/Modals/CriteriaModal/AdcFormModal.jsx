6e01681faf0b422537d5971cfd26e94ca31817b4
DySn0512 <145582634+DySn0512@users.noreply.github.com>
2024-12-04 04:59:48 +0700

    update: criteria

--- frontend/src/Modals/CriteriaModal/AdcFormModal.jsx
+++ frontend/src/Modals/CriteriaModal/AdcFormModal.jsx
@@ -67,14 +67,14 @@ const AdcFormModal = ({ adcId, adcToEdit, setAdcs, onClose, isEditing }) => {
                     <div className="bg-red-500 text-white p-6 rounded-md shadow-lg max-w-xs w-full text-center">
                         <p>{error}</p>
                         <button onClick={() => setError("")} className="mt-4 bg-white text-red-500 px-4 py-2 rounded">
-                            Close
+                            Đóng
                         </button>
                     </div>
                 </div>
             )}
             <div className="bg-white p-6 rounded shadow-lg max-w-md w-full">
                 <h2 className="text-2xl font-bold mb-4 text-center">
-                    {isEditing ? "Edit Admission Block" : "Add Admission Block"}
+                    {isEditing ? "Cập nhật khối xét tuyển" : "Thêm khối xét tuyển"}
                 </h2>
                 <form onSubmit={handleSubmit} className="space-y-4">
                     <input
@@ -105,14 +105,14 @@ const AdcFormModal = ({ adcId, adcToEdit, setAdcs, onClose, isEditing }) => {
 
                     <div className="flex justify-between mt-4">
                         <button type="submit" className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600">
-                            {isEditing ? "Update" : "Add"}
+                            {isEditing ? "Cập nhật" : "Thêm"}
                         </button>
                         <button
                             type="button"
                             onClick={onClose}
                             className="bg-gray-500 text-white py-2 px-4 rounded hover:bg-gray-600"
                         >
-                            Close
+                            Đóng
                         </button>
                     </div>
                 </form>
--- frontend/src/pages/CriteriaPage/AdcList.jsx
+++ frontend/src/pages/CriteriaPage/AdcList.jsx
@@ -63,7 +63,7 @@ const AdcList = ({ adcs, setAdcs }) => {
     // Đếm số lượng người dùng theo role
     return (
         <div className="max-w-4xl mx-auto p-6 bg-white rounded-lg shadow-md">
-            <h2 className="text-center text-2xl font-semibold mb-6">Admission Criteria List</h2>
+            <h1 className="text-3xl font-bold mb-6 text-center text-blue-600">Quản lý diện xét tuyển</h1>
             {/* Hiển thị số lượng người dùng */}
             <div className="flex space-x-2">
                 {/* Nút Add */}
@@ -71,16 +71,16 @@ const AdcList = ({ adcs, setAdcs }) => {
                     onClick={handleAddAdc}
                     className="bg-green-500 text-white py-1 px-3 rounded hover:bg-green-600 mb-4"
                 >
-                    Add
+                    Thêm
                 </button>
-                <p className="p-2 pr-8 border border-gray-300 rounded mb-4">Total: {adcCount}</p>
+                <p className="p-2 pr-8 border border-gray-300 rounded mb-4">Tổng: {adcCount}</p>
                 {/* Thanh tìm kiếm */}
                 <div className="flex space-x-2 justify-end mb-4">
                     <input
                         type="text"
                         value={searchQuery}
                         onChange={handleSearchChange}
-                        placeholder="Search by code name..."
+                        placeholder="Tìm kiếm theo ID diện xét tuyển"
                         className="p-2 pr-8 border appearance-none border-gray-300 rounded"
                     />
                 </div>
@@ -99,19 +99,19 @@ const AdcList = ({ adcs, setAdcs }) => {
                                     onClick={() => handleEdit(adc)}
                                     className="bg-yellow-500 text-white py-1 px-3 rounded hover:bg-yellow-600 mr-2"
                                 >
-                                    Edit
+                                    Cập nhật
                                 </button>
-                                <button
+                                {/* <button
                                     onClick={() => handleDelete(adc)}
                                     className="bg-red-500 text-white py-1 px-3 rounded hover:bg-red-600 mr-2"
                                 >
                                     Delete
-                                </button>
+                                </button> */}
                                 <button
                                     onClick={() => handleMoreClick(adc)}
                                     className="bg-blue-500 text-white py-1 px-3 rounded hover:bg-blue-600"
                                 >
-                                    More
+                                    Xem thêm
                                 </button>
                             </div>
                         </li>
