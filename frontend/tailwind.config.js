2c0019c6a9fb90261d0ea07fe1774b8e8adf0fff
DySn0512 <145582634+DySn0512@users.noreply.github.com>
2024-12-06 16:30:07 +0700

    update: tailwind

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
     content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            keyframes: {
+                popup: {
+                    "0%": { transform: "scale(0.9)", opacity: "0" },
+                    "100%": { transform: "scale(1)", opacity: "1" },
+                },
+            },
+            animation: {
+                popup: "popup 0.3s ease-out",
+            },
+        },
     },
     plugins: [],
 };
